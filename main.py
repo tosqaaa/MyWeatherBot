f3d22c68b671496340cd7e57f61496a317a12913
tosqaaa <ilyarudakov.by@gmail.com>
2023-02-11 15:01:11 +0300

    version 1.0.0

---
+++ main.py
@@ -0,0 +1,108 @@
+# import os
+# import pip
+# from background import keep_alive
+# pip.main(['install','pytelegrambotapi'])
+import telebot
+import datetime
+import json
+from telebot import types
+from req import req
+from db import create_db, insert_db
+from keys import *
+from messages import *
+from keyboards import *
+
+bot = telebot.TeleBot(TELEGRAM_BOT_API, parse_mode="html")
+
+
+@bot.message_handler(commands=['language', 'lang', 'l'], content_types=['text'])
+def change_language(message):
+    bot.send_message(message.chat.id, text=CHANGE_LANGUAGE,
+                     reply_markup=KEYBOARD_LANGUGE)
+    
+
+
+@bot.message_handler(content_types=['text'])
+def choose_language(message):
+    if message.text == 'Русский язык':
+        bot.send_message(message.chat.id, text=CHOSEN_RU,
+                         reply_markup=KEYBOARD_GET_WEATHER_RU)
+        bot.delete_message(message.chat.id, message.message_id)
+
+    elif message.text == "Беларуская мова":
+        bot.send_message(message.chat.id, text=CHOSEN_BY,
+                         reply_markup=KEYBOARD_GET_WEATHER_BY)
+        bot.delete_message(message.chat.id, message.message_id)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, text=START_MESSAGE_RU.format(
+        message.from_user.first_name), reply_markup=KEYBOARD_GET_WEATHER_RU)
+
+
+@bot.message_handler(content_types=['location'])
+def location(message):
+
+    create_db()
+
+    insert_db(user_id=message.from_user.id,
+              user_name=message.from_user.first_name,
+              user_nickname=message.from_user.username)
+
+    coord = {'coord': []}
+    coord['coord'].append({
+        "lat": message.location.longitude,
+        "lon": message.location.latitude
+    })
+
+    with open('coord.json', 'w') as file:
+        json.dump(coord, file, indent=4, ensure_ascii=False)
+
+    req()
+
+    with open('resp_to_print.json') as json_file:
+        data = json.load(json_file)
+
+    with open('resp_to_print.json', 'w') as file:
+        pass
+
+    if message.location is not None and data['main']['status'] == 'true':
+
+        bot.send_message(message.chat.id, text=LOADING_MESSAGE_RU)
+
+        bot.send_message(message.chat.id,
+                         text=DATA_MESSAGE_RU.format(
+                             data['main']['country'],
+                             data['main']['city'],
+                             datetime.datetime.fromtimestamp(
+                                 int(message.date + 10800)).strftime('%d-%m %H:%M'),
+                             str(data['main']['rise'])[10:],
+                             str(data['main']['set'])[10:],
+                             str(data['now']['weather']).capitalize(),
+                             round(data['now']['temp'], 1),
+                             data['now']['humidity'],
+                             round(data['now']['wind'], 1),
+                             str(data['d1']['weather']).capitalize(),
+                             round(data['d1']['temp'], 1),
+                             data['d1']['humidity'],
+                             round(data['d1']['wind'], 1),
+                             str(data['d2']['weather']).capitalize(),
+                             round(data['d2']['temp'], 1),
+                             data['d2']['humidity'],
+                             round(data['d2']['wind'], 1)))
+    else:
+        bot.send_message(message.chat.id, text=ERROR_MESSAGE_RU)
+
+
+def main():
+    try:
+        print("Running...")
+    # keep_alive()
+        bot.polling(none_stop=True, allowed_updates=True)
+    except:
+        bot.polling(none_stop=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ req.py
@@ -0,0 +1,66 @@
+import json
+import requests
+import datetime
+from keys import *
+
+
+def req():
+    with open('coord.json') as json_file:
+        data = json.load(json_file)
+        for item in data['coord']:
+            lat = item['lat']
+            lon = item['lon']
+
+    future_weather_params = {"lat": f"{lat}", "lon": f"{lon}", "appid": API_KEY_OW,
+                             "units": "metric", "cnt": f"{9}", "lang": 'ru'}
+    current_weather_params = {"lat": f"{lat}", "lon": f"{lon}", "appid": API_KEY_OW,
+                              "units": "metric", "lang": 'ru'}
+    with open("coord.json", "w") as f:
+        pass
+
+    response = requests.get(
+        FUTURE_WEATHER_LINK,
+        params=future_weather_params, headers=HEADERS)
+    response_now = requests.get(
+        CURRENT_WEATHER_LINK,
+        params=current_weather_params, headers=HEADERS)
+
+    arr = {'main': {},
+           'now': {},
+           'd1': {},
+           'd2': {}
+           }
+    if response.status_code:
+        arr['main']['status'] = 'true'
+        arr['main']['country'] = response.json()['city']['country']
+        arr['main']['city'] = response_now.json()['name']
+        arr['main']['rise'] = datetime.datetime.fromtimestamp(
+            int(response_now.json()['sys']['sunrise']+10800)).strftime('%d-%m-%Y %H:%M')
+        arr['main']['set'] = datetime.datetime.fromtimestamp(
+            int(response_now.json()['sys']['sunset']+10800)).strftime('%d-%m-%Y %H:%M')
+
+        arr['now']['weather'] = response_now.json()['weather'][0]['description']
+        arr['now']['temp'] = response_now.json()['main']['temp']
+        arr['now']['humidity'] = response_now.json()['main']['humidity']
+        arr['now']['wind'] = response_now.json()['wind']['speed']
+
+        arr['d1']['time'] = datetime.datetime.fromtimestamp(
+            int(response.json()['list'][1]['dt']+10800)
+        ).strftime('%d-%m-%Y %H:%M')
+        arr['d1']['weather'] = response.json(
+        )['list'][1]['weather'][0]['description']
+        arr['d1']['temp'] = response.json()['list'][1]['main']['temp']
+        arr['d1']['humidity'] = response.json()['list'][1]['main']['humidity']
+        arr['d1']['wind'] = response.json()['list'][1]['wind']['speed']
+
+        arr['d2']['time'] = datetime.datetime.fromtimestamp(
+            int(response.json()['list'][8]['dt']+10800)
+        ).strftime('%d-%m-%Y %H:%M')
+        arr['d2']['weather'] = response.json(
+        )['list'][8]['weather'][0]['description']
+        arr['d2']['temp'] = response.json()['list'][8]['main']['temp']
+        arr['d2']['humidity'] = response.json()['list'][8]['main']['humidity']
+        arr['d2']['wind'] = response.json()['list'][8]['wind']['speed']
+
+        with open('resp_to_print.json', 'w') as file:
+            json.dump(arr, file, indent=4, ensure_ascii=False)
