eb4d7d3abf9103ecbdec8d20e7e9caaf2812b60d
tosqaaa <ilyarudakov.by@gmail.com>
2023-02-17 15:51:18 +0300

    language in db

--- my-weather-bot-master/db.py
+++ my-weather-bot-master/db.py
@@ -2,21 +2,63 @@ import sqlite3
 
 
 def create_db():
-    conn = sqlite3.connect('users.db', check_same_thread=False)
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users(
-        count INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
-        user_id INTEGER UNIQUE NOT NULL,
+    try:
+        conn = sqlite3.connect('users.db', check_same_thread=False)
+        cursor = conn.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER PRIMARY KEY UNIQUE NOT NULL,
         user_name TEXT NOT NULL,
-        user_nick TEXT);
-    """)
-    conn.commit()
+        user_nick TEXT,
+        user_language TEXT);
+        """)
+        conn.commit()
+    except sqlite3.Error as er:
+        print(er)
+    finally:
+        conn.close()
 
 
 def insert_db(user_id: int, user_name: str, user_nickname: str):
-    conn = sqlite3.connect('users.db', check_same_thread=False)
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect('users.db', check_same_thread=False)
+        cursor = conn.cursor()
 
-    cursor.execute("INSERT or IGNORE INTO users(user_id, user_name, user_nick) VALUES (?,?,?)", (user_id, user_name,
+        cursor.execute("INSERT or IGNORE INTO users(user_id, user_name, user_nick) VALUES (?,?,?)", (user_id, user_name,
                                                                                                  user_nickname))
-    conn.commit()
+        conn.commit()
+        cursor.close()
+    except sqlite3.Error as er:
+        print(er)
+    
+    finally:
+        conn.close()
+    
+def change_language_in_db(user_id: int, user_language: str):
+    try:
+        conn = sqlite3.connect('users.db', check_same_thread=False)
+        cursor = conn.cursor()
+    
+        cursor.execute("""UPDATE users SET user_language = ? WHERE user_id = ?""",(user_language, user_id))
+        conn.commit()
+    
+        cursor.close()
+    except sqlite3.Error as er:
+        print(er)
+    finally:
+        conn.close()
+        
+def get_language_from_user(user_id: int):
+    try:
+        conn = sqlite3.connect('users.db', check_same_thread=False)
+        cursor = conn.cursor()
+        
+        cursor.execute("""SELECT * FROM users where user_id = ?""",(user_id, ))
+        
+        selected_language = cursor.fetchone()
+        return selected_language[3]
+        
+    except sqlite3.Error as er:
+        print(er)
+    finally:
+        cursor.close()
+        conn.close()
--- my-weather-bot-master/main.py
+++ my-weather-bot-master/main.py
@@ -2,68 +2,76 @@
 # import pip
 # from background import keep_alive
 # pip.main(['install','pytelegrambotapi'])
-import telebot
-import datetime
-import json
-from telebot import types
+import logging
+from aiogram.bot import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram import types
+from aiogram.utils import executor
+import datetime, json
+import time
 from req import req
-from db import create_db, insert_db
+from db import *
 from keys import *
 from messages import *
 from keyboards import *
 from translate import trans_to_be
 
-bot = telebot.TeleBot(TELEGRAM_BOT_API, parse_mode="html")
+bot = Bot(token=TELEGRAM_BOT_API, parse_mode="html")
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(commands=['help'])
-def help_message(message):
-    bot.send_message(message.chat.id, text=HELP_MESSAGE)
-
-
-@bot.message_handler(commands=['language', 'lang', 'l'])
-def change_language(message):
-    bot.send_message(message.chat.id, text=CHANGE_LANGUAGE,
-                     reply_markup=KEYBOARD_LANGUGE)
-
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, text=START_MESSAGE_RU.format(
-        message.from_user.first_name), reply_markup=KEYBOARD_LANGUGE)
-
-
-@bot.message_handler(content_types=['text'])
-def choose_language(message):
-    if message.text == 'Русский язык':
-        bot.send_message(message.chat.id, text=CHOSEN_RU,
-                         reply_markup=KEYBOARD_GET_WEATHER_RU)
-        bot.delete_message(message.chat.id, message.message_id)
-
-        with open("lang.json", "w", encoding="utf-8") as file:
-            json.dump("ru", file)
-
-    elif message.text == "Беларуская мова":
-        bot.send_message(message.chat.id, text=CHOSEN_BY,
-                         reply_markup=KEYBOARD_GET_WEATHER_BY)
-        bot.delete_message(message.chat.id, message.message_id)
-
-        with open("lang.json", "w", encoding="utf-8") as file:
-            json.dump("by", file)
-
-
-@bot.message_handler(content_types=['location', 'text'])
-def location(message):
-
-    with open("lang.json", "r") as file:
-        language = json.load(file)
-
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await bot.send_message(message.chat.id, text=START_MESSAGE_RU.format(message.from_user.first_name), reply_markup=KEYBOARD_LANGUGE)
+    
     create_db()
 
     insert_db(user_id=message.from_user.id,
               user_name=message.from_user.first_name,
               user_nickname=message.from_user.username)
-    print(message.location.longitude, message.location.latitude)
+    
+@dp.message_handler(commands=['my_lang'])
+async def get_language(message: types.Message):
+    await bot.send_message(message.chat.id, text=get_language_from_user(user_id= message.from_user.id))
+    
+@dp.message_handler(commands=['language', 'lang', 'l'])
+async def change_language(message):
+    await bot.send_message(message.chat.id, text=CHANGE_LANGUAGE,
+                     reply_markup=KEYBOARD_LANGUGE)
+    
+@dp.message_handler(commands=['time'])
+async def get_date(message: types.Message):
+    await bot.send_message (message.from_user.id, text=message.date)
+    
+    
+@dp.message_handler(commands=['help'])
+async def help_message(message: types.Message):
+    await message.answer(HELP_MESSAGE)
+    
+    
+@dp.message_handler(content_types=['text'])
+async def choose_language(message: types.Message):
+    if message.text == 'Русский язык':
+        await bot.send_message(message.chat.id, text=CHOSEN_RU,
+                         reply_markup=KEYBOARD_GET_WEATHER_RU)
+        await message.delete()
+        
+        change_language_in_db(user_language="ru", user_id=message.from_user.id)
+        
+    elif message.text == "Беларуская мова":
+        await bot.send_message(message.chat.id, text=CHOSEN_BY,
+                         reply_markup=KEYBOARD_GET_WEATHER_BY)
+        await message.delete()
+        
+        change_language_in_db(user_language="by", user_id=message.from_user.id)
+        
+
+
+@dp.message_handler(content_types=['location', 'text'])
+async def location(message):
+        
+    # print(message.location.longitude, message.location.latitude)
+    
     coord = {'coord': []}
     coord['coord'].append({
         "lat": message.location.latitude,
@@ -83,15 +91,14 @@ def location(message):
 
     if message.location is not None and data['main']['status'] == 'true':
 
-        if language == "by":
-            bot.send_message(message.chat.id, text=LOADING_MESSAGE_BY)
+        if get_language_from_user(message.from_user.id) == "by":
+            await bot.send_message(message.chat.id, text=LOADING_MESSAGE_BY)
 
-            bot.send_message(message.chat.id,
+            await bot.send_message(message.chat.id,
                              DATA_MESSAGE_BY.format(
                                  data['main']['country'],
                                  trans_to_be(data['main']['city']),
-                                 datetime.datetime.fromtimestamp(
-                                     int(message.date + 10800)).strftime('%d-%m %H:%M'),
+                                 message.date.strftime('%d.%m.%Y'), 
                                  str(data['main']['rise'])[10:],
                                  str(data['main']['set'])[10:],
                                  trans_to_be(
@@ -110,14 +117,13 @@ def location(message):
                                  data['d2']['humidity'],
                                  round(data['d2']['wind'], 1)), reply_markup=KEYBOARD_LANGUGE)
         else:
-            bot.send_message(message.chat.id, text=LOADING_MESSAGE_RU)
+            await bot.send_message(message.chat.id, text=LOADING_MESSAGE_RU)
 
-            bot.send_message(message.chat.id,
+            await bot.send_message(message.chat.id,
                              DATA_MESSAGE_RU.format(
                                  data['main']['country'],
                                  data['main']['city'],
-                                 datetime.datetime.fromtimestamp(
-                                     int(message.date + 10800)).strftime('%d-%m %H:%M'),
+                                 message.date.strftime('%d.%m.%Y'),    
                                  str(data['main']['rise'])[10:],
                                  str(data['main']['set'])[10:],
                                  str(data['now']['weather']).capitalize(),
@@ -133,21 +139,31 @@ def location(message):
                                  data['d2']['humidity'],
                                  round(data['d2']['wind'], 1)), reply_markup=KEYBOARD_LANGUGE)
     else:
-        bot.send_message(message.chat.id, text=ERROR_MESSAGE_RU)
+        await bot.send_message(message.chat.id, text=ERROR_MESSAGE_RU)
         
     with open('lang.json', 'w') as file:
         pass
 
 
+
+    
+
+    
+
+
 def main():
     try:
-        print("Running...")
-
+        print("Running")
         # keep_alive()
-        bot.polling(none_stop=True)
-    except:
-        bot.polling(none_stop=True)
+        executor.start_polling(dp,  skip_updates=True)
+    except Exception() as ex:
+        print(type(ex))
 
-
+        
+        
+        
 if __name__ == "__main__":
     main()
+    
+        
+    
